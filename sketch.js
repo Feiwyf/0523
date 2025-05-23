a6cf48909c8fdc47572c12c4e325c108d0e202aa
Feiwyf <411730525@o365.tku.edu.tw>
2025-05-23 10:48:45 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,15 @@ let video;
 let faceMesh;
 let predictions = [];
 const indices = [
-  61,146,91,181,84,17,314,405,321,375,291,308,324,318,402,317,14,87,178,88,
-  95,185,40,39,37,0,267,269,270,409,415,310,311,312,13,82,81,42,183,78
+  409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291,
+  76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184
 ];
 
 function setup() {
   createCanvas(640, 480);
+  // 畫布置中
+  let canvas = createCanvas(640, 480);
+  canvas.parent(document.body);
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
@@ -25,14 +28,6 @@ function draw() {
 
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
-
-    // 畫出所有特徵點（debug用）
-    fill(0, 255, 0);
-    noStroke();
-    for (let i = 0; i < keypoints.length; i++) {
-      const [x, y] = keypoints[i];
-      ellipse(x, y, 5, 5);
-    }
 
     // 畫紅色粗線
     stroke(255, 0, 0);
