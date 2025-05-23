7bccda02720be012ecd0e5b09d8c9d92fa30707e
Feiwyf <411730525@o365.tku.edu.tw>
2025-05-23 10:30:45 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,7 +1,39 @@
+let video;
+let faceMesh;
+let predictions = [];
+const indices = [
+  409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291,
+  76,77,90,180,85,16,315,404,320,307,306,408,304,303,302,11,72,73,74,184
+];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(width, height);
+  video.hide();
+  faceMesh = ml5.faceMesh(video, modelReady);
+}
+
+function modelReady() {
+  console.log('FaceMesh ready!');
+  faceMesh.on('predict', results => predictions = results);
 }
 
 function draw() {
-  background(220);
+  background(255);
+  image(video, 0, 0, width, height);
+
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+    stroke(255, 0, 0);
+    strokeWeight(15);
+    noFill();
+    beginShape();
+    for (let i = 0; i < indices.length; i++) {
+      const idx = indices[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
+    }
+    endShape();
+  }
 }
